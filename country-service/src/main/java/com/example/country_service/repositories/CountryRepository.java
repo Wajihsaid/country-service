bdafb40da52fbc3ea4e62978c4c70b351c9725d0
Wajihsaid <153779377+Wajihsaid@users.noreply.github.com>
2025-10-22 21:11:52 +0100

    Delete country-service directory

--- country-service/src/main/java/com/example/country_service/repositories/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.country_service.repositories;
-import com.example.country_service.beans.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CountryRepository extends JpaRepository<Country,Integer> {
-
-}
